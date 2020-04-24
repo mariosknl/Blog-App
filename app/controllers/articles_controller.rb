1ae0e0f37011c8e688374838ac8524f5804e0350
Kubilay Çağlayan <kblycaglayan@gmail.com>
2020-04-24 12:33:29 +0300

    Article params adding method changed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -12,9 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
-    @article.body = params[:article][:body]
+    @article = Article.new(article_params)
     @article.save
 
     redirect_to article_path(@article)
