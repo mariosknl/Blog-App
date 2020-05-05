ee591ef1c994e6ae89624ce0e0855d8cda0ffaf5
Kubilay <kblycaglayan@gmail.com>
2020-05-05 20:08:47 +0300

    User seed decommented

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Author.create(username: "admin", email: "example@mail.com", password: "admin" )
+Author.create(username: "admin", email: "example@mail.com", password: "admin" )
 
 bands = {
   'Led Zeppelin' => 'Stairway to Heaven',
